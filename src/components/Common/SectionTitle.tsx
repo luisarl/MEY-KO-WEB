35e21a58efd442c4b91f24ba62ea1412e2de4da0
Luis Romero <luis.ang.romero@gmail.com>
2025-01-20 22:53:33 -0400

    primera version features

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -1,12 +1,12 @@
 const SectionTitle = ({
   title,
   paragraph,
-  width = "570px",
+  // width = "570px",
   center,
   mb = "100px",
 }: {
   title: string;
-  paragraph: string;
+  // paragraph: string;
   width?: string;
   center?: boolean;
   mb?: string;
@@ -14,15 +14,12 @@ const SectionTitle = ({
   return (
     <>
       <div
-        className={`w-full ${center ? "mx-auto text-center" : ""}`}
-        style={{ maxWidth: width, marginBottom: mb }}
+        className={`w-full ${center ? "mx-auto text-left" : ""}`}
+        style={{ marginBottom: mb }}
       >
         <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
-          {paragraph}
-        </p>
       </div>
     </>
   );
--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,12 +8,12 @@ const Features = () => {
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="¿Por qué elegir M-KARE?"
+            // paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
             center
           />
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-4">
             {featuresData.map((feature) => (
               <SingleFeature key={feature.id} feature={feature} />
             ))}

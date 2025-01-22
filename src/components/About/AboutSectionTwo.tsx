936dc2301febe18f1a0d43d4c69b9cef9dfab0de
Luis Romero <luis.ang.romero@gmail.com>
2025-01-22 19:33:08 -0400

    ajuste seccion features2 flex-row

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -5,7 +5,7 @@ const AboutSectionTwo = () => {
   return (
     <section className="py-16 py-10 md:py-10 lg:py-10">
       <div className="container">
-        <div className="-mx-4 flex flex-wrap items-center">
+        <div className="-mx-4 flex flex-row items-center">
           <div className="w-full px-4 lg:w-1/2">
             <div
               className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
@@ -25,34 +25,28 @@ const AboutSectionTwo = () => {
               />
             </div>
           </div>
-          <div className="w-full px-4 lg:w-1/2">
+          <div className="w-full px-4 lg:w-1/3">
             <SectionTitle
               title="Descarga Nuestra App"
               paragraph="Una forma innovadora de llevar tu historia medica."
               mb="44px"
             />
-            <div
-              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
+            </div>
+            
+            <div className="w-full px-4 lg:w-1/3">
               <Image
                 src="/images/about/BOTON-GOOGLE.png"
                 alt="playstore"
                 width={400}
                 height={200}
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
               />
               <Image
                 src="/images/about/BOTON-APPLE.png"
                 alt="appstore"
                 width={400}
                 height={200}
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
               />
-
             </div>
-            
-          </div>
         </div>
       </div>
     </section>

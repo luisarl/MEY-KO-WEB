877fd0e2c313be0754e4369815461864a94de62f
Luis Romero <luis.ang.romero@gmail.com>
2025-01-28 09:15:25 -0400

    ajuste responsive abount2

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -5,7 +5,7 @@ const AboutSectionTwo = () => {
   return (
     <section className="py-10 md:py-10 lg:py-10">
       <div className="container">
-        <div className="-mx-4 flex flex-row items-center">
+        <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/3">
             <div
               className="relative mx-auto mb-12 aspect-[25/24] max-w-[400px] text-center lg:m-0"
